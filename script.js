37c4b55882e73218f2bdca3264965c9ae62ee80f
merahab <ts.baran6161@gmail.com>
2024-03-12 19:54:27 +0300

    Yks gün sayacı

---
+++ script.js
@@ -0,0 +1,23 @@
+function daysUntilYks(){
+    const yksDate = new Date("2024-06-15")
+
+    const currentDate = new Date()
+
+    const timeDiff = yksDate.getTime() - currentDate.getTime();
+    const daysRemaining = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+    return daysRemaining;
+
+
+}
+const daysRemain = document.querySelector('.days')
+
+
+
+
+    daysRemain.innerHTML = `Yks'ye kalan süre <br> ${daysUntilYks()} <br> gün kaldı`;
+
+
+setTimeout(function() {
+    updateDaysRemain();
+}, 864000);
